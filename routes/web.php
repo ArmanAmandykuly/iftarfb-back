e9d0d5a67969f0352c977ab81a86931c76feba08
ArmanAmandykuly <62884333+ArmanAmandykuly@users.noreply.github.com>
2024-09-12 16:06:27 +0600

    added api route

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\GoogleSheetsController;
 use Illuminate\Support\Facades\Route;
 use Google\Service\Sheets;
 
-Route::post('/', [GoogleSheetsController::class, 'appendRow']);
+Route::post('/api', [GoogleSheetsController::class, 'appendRow']);

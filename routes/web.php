dcbfc845c3294adec1defafb5f11a206d508af91
ArmanAmandykuly <62884333+ArmanAmandykuly@users.noreply.github.com>
2024-09-12 16:34:46 +0600

    changed post to get

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\GoogleSheetsController;
 use Illuminate\Support\Facades\Route;
 use Google\Service\Sheets;
 
-Route::post('/api', [GoogleSheetsController::class, 'appendRow']);
+Route::get('/api', [GoogleSheetsController::class, 'appendRow']);
